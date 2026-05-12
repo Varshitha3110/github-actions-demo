3b059fe53b52250a35510e4ea7e53898123bea0b
Koguru Varshitha <varshitha3110@gmail.com>
2026-05-12 14:22:11 +0530

    CI/CD Pipeline Completed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Running Successfully ');
+    res.send('CI/CD Pipeline Running Successfully with the changes');
 });
 
 app.listen(3000, () => {
