ef3128c4c8576e472a903d2c4bf74cf69add7e8a
Koguru Varshitha <varshitha3110@gmail.com>
2026-05-12 12:55:48 +0530

    Modified display text

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Running Successfully with docker');
+    res.send('CI/CD Pipeline Running Successfully using github actions');
 });
 
 app.listen(3000, () => {
